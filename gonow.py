aa5e7641021812a3c181cc3dcaca1ceb03c9ea12
grafst <grafst@users.noreply.github.com>
2024-03-25 09:40:53 +0100

    run forever

--- gonow.py
+++ gonow.py
@@ -1,5 +1,5 @@
 
-import hours,colors
+import hours,colors,time
 from led import pixels
 from icecream import ic
 
@@ -7,11 +7,13 @@ places=["282655576","4143672011","4153947940","3913401397"]
 lat=47.49973
 lon=8.72413
 
-for i,place in enumerate(places):
-    oh=hours.get_by_id(place,lat,lon)
-    #print(f"{place} => open: {oh.is_open()}, next: {oh.next_change()}")
-    ic(oh)
-    if not oh["is_open"]:
-        pixels[i]=colors.RED.rgb
-    else:
-        pixels[i]=colors.GREEN.rgb
+while True:
+    for i,place in enumerate(places):
+        oh=hours.get_by_id(place,lat,lon)
+        #print(f"{place} => open: {oh.is_open()}, next: {oh.next_change()}")
+        ic(oh)
+        if not oh["is_open"]:
+            pixels[i]=colors.RED.rgb
+        else:
+            pixels[i]=colors.GREEN.rgb
+    time.sleep(60)
