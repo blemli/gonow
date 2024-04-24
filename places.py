630c5679f8623a04daa9c702e813686255d958de
grafst <grafst@users.noreply.github.com>
2024-04-24 18:01:23 +0200

    add places file :poop:

---
+++ places.py
@@ -0,0 +1,17 @@
+places=[
+    "Fahrenheit",
+    "Schwimmbad Geiselweid",
+    "Fabi Imbiss",
+    "OBI Winterthur",
+    "Radwerk",
+    "wintHAIRthur",
+    "Diethelm",
+    "Cameo",
+    "Hauptpost 8400 Winterthur",
+    "Stadtkirche",
+    "Analog Attack",
+    "Buch am Platz",
+    "Stadtmuur",
+    "Hako",
+    "Kletterhalle 6a Plus"
+]
