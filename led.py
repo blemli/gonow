9968fa1fec668c80898377e9391cc97ab87eeaf3
grafst <grafst@users.noreply.github.com>
2024-05-01 10:00:09 +0200

    fix regression
    should have comitted

--- led.py
+++ led.py
@@ -3,10 +3,15 @@ import neopixel
 
 import colors
 
-LED_COUNT=4
+LED_COUNT=18
 DATA_PIN = board.D18
 pixels = neopixel.NeoPixel(DATA_PIN, LED_COUNT, brightness=0.05, pixel_order=neopixel.RGB)
 
 if __name__=="__main__":
-    for i in range(LED_COUNT):
-        pixels[i]=colors.RED.rgb
+    import time
+    cols=[colors.GREEN.rgb,colors.RED.rgb,colors.WHITE.rgb,colors.PURPLE.rgb,colors.YELLOW.rgb]
+    while True:
+        for color in cols:
+            for i in range(LED_COUNT):
+                pixels[i]=color
+                time.sleep(0.1)
