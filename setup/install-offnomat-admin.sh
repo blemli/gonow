70b6896fe9078f0f5bb0d8fab5e715bd7b6f2b8b
grafst <grafst@users.noreply.github.com>
2024-06-27 11:44:00 +0200

    actually implement admin service installer :bug:

--- setup/install-offnomat-admin.sh
+++ setup/install-offnomat-admin.sh
@@ -2,10 +2,10 @@ set -e
 name=$(basename $(git rev-parse --show-toplevel))
 
 
-echo "~~~ install service ~~~~"
-sudo cp -f setup/$name.service /lib/systemd/system/$name.service
-sudo chmod 644 /lib/systemd/system/$name.service
+echo "~~~ install $name admin service ~~~~"
+sudo cp -f setup/$name-admin.service /lib/systemd/system/$name-admin.service
+sudo chmod 644 /lib/systemd/system/$name-admin.service
 sudo systemctl daemon-reload
-sudo systemctl enable $name.service
-sudo service $name start
+sudo systemctl enable $name-admin.service
+sudo service $name-admin start
 
